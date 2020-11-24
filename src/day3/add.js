ccf3b1bbf2c2550e5dfc4d344020abd47291773f
Hoyoung Jung <honux77@gmail.com>
2020-11-24 12:10:42 +0900

    Simple ts add function

---
+++ src/day3/add.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+var a = 10;
+var b = 20;
+console.log(add(a, b));
---
+++ src/day3/add.ts
@@ -0,0 +1,8 @@
+function add(a:number, b:number): Number {
+    return a + b;
+}
+
+const a = 10;
+const b = 20;
+
+console.log(add(a, b));

560d2634a4b9488279fd20a72f3147f85843a7da
Hoyoung Jung <honux77@gmail.com>
2020-12-07 11:20:19 +0900

    add void return type / format src

--- day6-src/1_todo/src/index.ts
+++ day6-src/1_todo/src/index.ts
@@ -1,11 +1,11 @@
-let todoItems: {id: number, title: string, done: boolean}[];
+let todoItems: { id: number; title: string; done: boolean }[];
 
 // api
 function fetchTodoItems(): any {
   const todos = [
-    { id: 1, title: '안녕', done: false },
-    { id: 2, title: '타입', done: false },
-    { id: 3, title: '스크립트', done: false },
+    { id: 1, title: "안녕", done: false },
+    { id: 2, title: "타입", done: false },
+    { id: 3, title: "스크립트", done: false },
   ];
   return todos;
 }
@@ -16,7 +16,7 @@ function fetchTodos() {
   return todos;
 }
 
-function addTodo(todo: {id: number, title: string, done: boolean}) {
+function addTodo(todo: { id: number; title: string; done: boolean }) {
   todoItems.push(todo);
 }
 
@@ -24,30 +24,39 @@ function deleteTodo(index: number) {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: {id: number, title: string, done: boolean}) {
+function completeTodo(
+  index: number,
+  todo: { id: number; title: string; done: boolean }
+): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo(): {id: number, title: string, done: boolean} {
+function logFirstTodo(): { id: number; title: string; done: boolean } {
   return todoItems[0];
 }
 
-function showCompleted(): {id: number, title: string, done: boolean}[] {
-  return todoItems.filter(item => item.done);
+function showCompleted(): { id: number; title: string; done: boolean }[] {
+  return todoItems.filter((item) => item.done);
 }
 
-function addTwoTodoItems(item1: {id: number, title: string, done: boolean}, item2: {id: number, title: string, done: boolean}) {
+function addTwoTodoItems(
+  item1: { id: number; title: string; done: boolean },
+  item2: { id: number; title: string; done: boolean }
+): void {
   addTodo(item1);
-  addTodo(item2);  
+  addTodo(item2);
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
 todoItems = fetchTodoItems();
-addTwoTodoItems({id: 10, title: 'hello', done: false}, {id: 11, title: 'mello', done: true});
+addTwoTodoItems(
+  { id: 10, title: "hello", done: false },
+  { id: 11, title: "mello", done: true }
+);
 log();
